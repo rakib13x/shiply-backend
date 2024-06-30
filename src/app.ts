42a38df7f47c7862744c1a72f84de8827be233c1
rakib13x <rakib13x@gmail.com>
2024-06-30 21:31:48 +0600

    not Found middleware added for route not found

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
@@ -17,5 +18,7 @@ app.use(cors({}));
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from server.');
 });
+
+app.use(notFound);
 
 export default app;
